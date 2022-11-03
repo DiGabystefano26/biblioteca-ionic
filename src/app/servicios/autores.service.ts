cffc15b959abb04dde87012adff6edcac6ef1476
Gabriela <gabrieladistefano1@gmail.com>
2022-11-03 16:32:42 -0300

    Tarea2_Ionic (parte 3)

--- src/app/servicios/autores.service.ts
+++ src/app/servicios/autores.service.ts
@@ -1,21 +1,20 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Libro } from '../interfaces/libro.interface';
+import { Autor } from '../interfaces/autor.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LibrosService {
+export class AutoresService {
 
-  url: string = "http://localhost:3000/libro";
+  private url: string = 'hhtp://localhost:3000/autor';
 
   constructor(
     private http: HttpClient
   ) { }
 
-  public get(): Observable<Libro[]> {
-    return this.http.get<Libro[]>(this.url);
+  public get(): Observable<Autor[]> {
+    return this.http.get<Autor[]>(this.url);
   }
 }
